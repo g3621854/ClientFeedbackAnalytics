3c454082531af1ec9074f8a016a33ab5ee9c3c90
g3621854 <g3621854@gmail.com>
2024-06-26 09:25:31 +0800

    刪除無作用的程式碼

--- googlemap_reviews/views/views_web.py
+++ googlemap_reviews/views/views_web.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, HttpResponse,redirect
 from googlemap_reviews import models
-from ..forms import KeyWordReviewsForm
+
 
 def googlemap_reviews(request):
     KeyWordReviews = models.KeyWordReviews.objects.all()
-    return render(request,'googlemap_reviews.html',{'KeyWordReviews':KeyWordReviews})
+    return render(request,'googlemap_reviews.html')
 
 # Create your views here.
